6bc3b1d725ab6f59f8ae61d7febd33f4b996c47a
vortexx <108076105+agreeed@users.noreply.github.com>
2024-09-29 12:16:50 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -8,4 +8,19 @@ function module.Parts:DisableCollision(part1, part2)
 	return NoCollision
 end
 
+module.Network = {}
+module.Network.NetworkAccess = function()
+	settings().Physics.AllowSleep = false
+	while game:GetService("RunService").RenderStepped:Wait() do
+		for _, Players in next, game:GetService("Players"):GetPlayers() do
+			if Players ~= game:GetService("Players").LocalPlayer then
+				Players.MaximumSimulationRadius = 0
+				sethiddenproperty(Players, "SimulationRadius", 0)
+			end
+		end
+		game:GetService("Players").LocalPlayer.MaximumSimulationRadius = math.pow(math.huge,math.huge)
+		setsimulationradius(math.huge)
+	end
+end
+
 return module
