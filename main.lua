b4be9a745b9c7ced9d34070314f0627e429da02f
vortexx <108076105+agreeed@users.noreply.github.com>
2024-09-29 12:37:40 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,8 +3,8 @@ local module = {}
 module.Parts = {}
 function module.Parts:DisableCollision(part1, part2)
 	local NoCollision = Instance.new("NoCollisionConstraint")
-	NoCollison.Part0 = part1
-	NoCollison.Part1 = part2
+	NoCollision.Part0 = part1
+	NoCollision.Part1 = part2
 	return NoCollision
 end
 
